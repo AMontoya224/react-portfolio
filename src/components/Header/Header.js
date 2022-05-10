23ce4615125966f844e8a9b263ce210489d15ad1
Andres Montoya Angulo <jmontoyaan@unsa.edu.pe>
2022-05-09 21:25:23 -0500

    portfolio

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>
+        Aqui estara mi Portafolio!!
+      </h1>
+      <span class="material-icons-round">engineering</span>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import './Home.css';
+
+
+function Home() {
+  return (
+    <div className="App">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet"></link>
+      <h1>
+        Aqui estara mi Portafolio!!
+      </h1>
+      <span class="material-icons-round">engineering</span>
+    </div>
+  );
+}
+
+export default Home;
